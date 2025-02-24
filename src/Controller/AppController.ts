a6d63ff8be0298503a5c5eb4baeb86bf03bba46c
Kevin <6215184+kevinkod@users.noreply.github.com>
2025-02-24 17:49:28 +0100

    Controller

---
+++ src/Controller/AppController.ts
@@ -0,0 +1,10 @@
+// import { Controller, Get } from 'jsr:@lockness/core';
+
+
+// @Controller('todo')
+// export class TodoController {
+//   @Get()
+//   findAll(): string {
+//     return 'This action returns all todo';
+//   }
+// }
