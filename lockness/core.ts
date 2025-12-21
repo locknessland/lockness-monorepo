c62a92fb54c149ea6356f4358bbc6c02ffaa6fa6
Kevin <6215184+kevinkod@users.noreply.github.com>
2025-12-21 16:26:22 +0100

    feat: introduce core classes for HTTP method decorators and Controller

---
+++ lockness/core.ts
@@ -0,0 +1,10 @@
+export class Controller { }
+export class Get { }
+export class Post { }
+export class Put { }
+export class Delete { }
+export class Patch { }
+export class Options { }
+export class Head { }
+export class Trace { }
+export class Connect { }
