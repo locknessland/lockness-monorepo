61901fed5e5b94fa823944aaf312e1dd1e22ec7b
Kevin <6215184+kevinkod@users.noreply.github.com>
2025-12-21 21:52:01 +0100

    feat: remove PostController

--- src/controller/post_controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Context } from 'lockness'
-
-@Controller('/post')
-export class PostController {
-    @Get('/')
-    index(c: Context) {
-        return c.json({ message: 'Hello from PostController' })
-    }
-}
