d9256482160547f8b484f12c3cd55a6868f66bcd
Kevin <6215184+kevinkod@users.noreply.github.com>
2025-12-22 14:27:58 +0100

    style: update production environment label console log styling

--- lockness/core/app.ts
+++ lockness/core/app.ts
@@ -158,7 +158,7 @@ export class App {
             'development'
         const isProd = env.toLowerCase() === 'production'
         const envLabel = isProd
-            ? '\x1b[43m\x1b[30m PRODUCTION \x1b[0m'
+            ? '\x1b[45m\x1b[37m\x1b[1m PRODUCTION \x1b[0m'
             : '\x1b[44m\x1b[37m DEVELOPMENT \x1b[0m'
 
         console.log(`
